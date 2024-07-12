8cbba5d145e6e72d81df8f0d713474b1922b574a
Ajay Panyala <ajay.panyala@gmail.com>
2024-07-12 00:02:24 -0700

    remove duplicate files

--- old_latex/tcgmsg.c
+++
@@ -1,9 +0,0 @@
-/* C TCGMSG example */
-int main(int argc, char **argv) {
-  tcg_pbegin(argc,argv);    /* start TCGMSG */
-  GA_Initialize();          /* start global arrays */
-  MA_Init(...);             /* start memory allocator** */
-  /* ... */                 /* do work */
-  GA_Terminate();           /* tidy up global arrays call */
-  tcg_pend();               /* tidy up TCGMSG */
-}
